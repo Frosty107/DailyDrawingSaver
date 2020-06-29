c0796164682ecc86f9adaaef5f265225ada057ab
Frosty107 <JadanH107@gmail.com>
2020-06-29 17:21:15 +1200

    Added Project into GitHub

---
+++ Daily Drawing App/Assets/Scripts/BackButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButton : MonoBehaviour
+{
+    [SerializeField] private string scene;
+
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
---
+++ Daily Drawing App/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    [SerializeField] private string scene;
+
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+}
