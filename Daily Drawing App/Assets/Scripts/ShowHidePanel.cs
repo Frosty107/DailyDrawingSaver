9c78012d4db8aa444ff5818d85b0a92451031e97
Frosty107 <JadanH107@gmail.com>
2020-07-02 15:31:48 +1200

    Fixed Camera image rotation

---
+++ Daily Drawing App/Assets/Scripts/ShowHidePanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHidePanel : MonoBehaviour
+{
+
+    public GameObject objectToTrack;
+
+    public void ChangePanelState()
+    {
+        objectToTrack.SetActive(objectToTrack.activeSelf ? false : true);
+
+        if (objectToTrack.activeSelf)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+}
